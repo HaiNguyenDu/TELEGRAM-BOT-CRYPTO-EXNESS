3498310d48afec980334499f2ad11cbee4741f3a
0914103372 <nguyenduyhair2004@gmail.com>
2024-12-13 08:53:59 +0700

    hotfix

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import { getAllTradeNow } from './helper/get-list-trade-now.js';
 const token = '7859913357:AAHoi3g1xc4Om7b_cZFbyGw11f0bNi4uKzs'.trim();
 export const bot = new TelegramBot(token, { polling: true });
 //ketnoidatabase
-// connectDb()
+connectDb()
 
-// botCommands(bot)
+botCommands(bot)
 
 
-console.log(await getAllTradeNow(110169204));
+
 
 
 
